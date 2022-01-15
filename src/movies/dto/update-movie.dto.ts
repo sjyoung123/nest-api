7c039bcf4f3e28749cd7720870b315f751ce5fcf
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-01-15 18:04:41 +0900

    Update dto&pipe

---
+++ src/movies/dto/update-movie.dto.ts
@@ -0,0 +1,17 @@
+// import { IsNumber, IsString } from 'class-validator';
+
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMovieDto } from './create-movie.dto';
+
+// export class UpdateMovieDto {
+//   @IsString()
+//   readonly title?: string;
+
+//   @IsNumber()
+//   readonly year?: number;
+
+//   @IsString({ each: true })
+//   readonly genres?: string[];
+// }
+
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
