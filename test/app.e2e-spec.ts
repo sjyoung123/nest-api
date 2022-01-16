cadbc20b429fa3b5a458c75808dfdf378a93277b
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-01-16 18:41:08 +0900

    Update e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -19,6 +26,54 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome nest api');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]); //movies 페이지 확인
+    });
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201); //movie 생성되는지 확인
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400); //잘못된 movie 생성되는지 확인
+    });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200); //생성된 movie 확인
+    });
+
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404); //404 확인
+    });
+
+    it('PATCH', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Update' })
+        .expect(200); //업데이트 되는지 확인
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200); //삭제되는지 확인
+    });
   });
 });
